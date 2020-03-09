b177348afb223eb164cfbd34cdb3803edbbe15de
“TiffanyCasey” <tiffanylynnecasey@gmail.com>
2020-03-09 00:54:29 -0400

    Added JS prompts and conditions for lower and uppercase and combined if statement on character length prompt to make the code more concise.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+/* 
+The Password generator will provide a password with 8-50 
+characters based on criteria the user specifies.
+*/
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -15,24 +20,23 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-
 // Determine how many characters user wants in password 
 var characters = prompt("How many characters would you like your password to contain?");
 
-if (characters < 8) { 
-  alert("Password length must be between 8-50 characters");
-} if  (characters > 51) {
-  alert("Password length must be between 8-50 characters");
-// } if (characters IS NOT A NUMBER) {// NEED TO DETERMINE HOW TO WRITE 
-//   alert("Password length must be between 8-50 numeric characters");
-} if (characters > 8) {
+// If outside 8-50 units, ask to enter a new number within these parameters 
+if (characters < 8 || characters > 50) {
+ alert("Password length must be between 8-50 characters");
+ prompt("How many characters would you like your password to contain?");
+ // If within 8-50 units, record the number 
+} else if (characters > 8 || characters < 51) {
   console.log(`I want ${characters} characters in my password`);
 }
+// NEED TO LOOP IF DON'T GIVE CORRECT # 
 
   
 // Determine how many characters user wants in special characters
 var specialCharacter = true; 
-var questionOne = prompt("Click OK to confirm if you would like to include special characters");
+var questionOne = confirm("Click OK to confirm if you would like to include special characters");
 
  if (questionOne === specialCharacter) {
   console.log("I do want special characters");
@@ -42,11 +46,42 @@ var questionOne = prompt("Click OK to confirm if you would like to include speci
 
 // Determine how many characters user wants numeric characters
 var numericCharacter = true;
-var quesitonTwo= prompt("Click OK to confirm if you would like to include numeric characters");
+var questionTwo= confirm("Click OK to confirm if you would like to include numeric characters");
 
 
-if (quesitonTwo === numericCharacter) {
+if (questionTwo === numericCharacter) {
   console.log("I do want numeric characters");
 } else {
   console.log("I do NOT want numeric characters");
 }
+
+// Determine if users would like lowercase characters 
+var lowerCase = true;
+var questionThree= confirm("Click OK to confirm if you would like to include lowercase characters");
+
+
+if (questionThree === lowerCase) {
+console.log("I do want lowercase characters");
+} else {
+  console.log("I do NOT want lowercase characters");
+}
+
+// Determine if users would like uppercase characters 
+var upperCase = true;
+var questionFour= confirm("Click OK to confirm if you would like to include uppercase characters");
+
+
+if (questionFour === upperCase) {
+console.log("I do want uppercase characters");
+} else {
+  console.log("I do NOT want uppercase characters");
+}
+
+
+
+// If neither, loop to ask the question again 
+alert("You have to at least choose one");
+
+console.log("I do NOT want numeric characters");
+
+// Generate Password 
