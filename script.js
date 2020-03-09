8ce5491393663c100ffafe2c4bf35ed17e09a167
“TiffanyCasey” <tiffanylynnecasey@gmail.com>
2020-03-08 22:23:44 -0400

    Added JS defining how many characters in password

--- script.js
+++ script.js
@@ -13,3 +13,19 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
+
+
+
+var characters = prompt("How many characters would you like your password to contain?");
+
+if (characters < 8) { 
+  alert("Password length must be between 8-50 characters");
+} if (characters > 51) {
+  alert("Password length must be between 8-50 characters");
+} if (characters > 8) {
+  console.log(`I want ${characters} characters in my password`);
+}
+  
+
+
