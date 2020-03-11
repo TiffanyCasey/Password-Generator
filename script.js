abfdac0737eb51504a282f5560e46cd301ba06b8
“TiffanyCasey” <tiffanylynnecasey@gmail.com>
2020-03-11 16:57:55 -0400

    updated brackets in the array

--- script.js
+++ script.js
@@ -10,23 +10,21 @@ var confirmNumericCharacter
 var confirmUpperCase
 var confirmLowerCase 
 
-// Variable Values 
-var number = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9");
-var specialChar = ("!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~");
-var alphaLower = ("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
-var alphaUpper = ("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+// Variable Values as an Array 
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 // Confirm length of password 
 var confirmLength = prompt("How many characters would you like your password to contain?");
 // If inside range, continue with password requirements 
 if (confirmLength >= 8 || confirmLength <= 50) {
   alert(`Your password will have ${confirmLength} characters`);
-}
+} else {
 // If outside range, ask to enter a different number 
-else {
   alert("Password length must be between 8-50 characters Try again");
 }
-  
   // If outside range, ask to enter a different number
   var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
   var confirmNumericCharacter= confirm("Click OK to confirm if you would like to include numeric characters");    
@@ -92,10 +90,11 @@ else {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-}
+
 
 // Return the value
-return password
+return password;
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
