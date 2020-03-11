426f96ac52b988f7ddc07b810bcadc3990d233d7
“TiffanyCasey” <tiffanylynnecasey@gmail.com>
2020-03-11 14:02:09 -0400

    adding psuedocode, added variable declaration, values and begun working on function

--- script.js
+++ script.js
@@ -1,87 +1,46 @@
-/* 
-The Password generator will provide a password with 8-50 
-characters based on criteria the user specifies.
-*/
+// The Password generator will provide a password with 8-50  characters based on criteria the user specifies.
 
-// Assignment Code
+// Variable Declaration 
+var confirmLength
+var confirmSpecialCharacter
+var confirmNumericCharacter
+var confirmUpperCase
+var confirmLowerCase 
+
+// Values 
+var number = "0123456789";
+var specialChar = "!$&()*+,-./:;<=>?@^_`{|}"
+var alphaLower = "abcdefghijklmnopqrstuvwxyz"
+var alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+// Assignment Code 
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// ORIGINAL CODE - REVISIT Write password to the #password input  
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
+// Begin function to generate the passcode 
+function writePassword() {
+  // Confirm length of password 
+  var confirmLength = prompt("How many characters would you like your password to contain?");
+    // If outside range, ask to enter a different number 
+    if (confirmLength < 8 || confirmLength > 51) {
+      alert("Password length must be between 8-50 characters Try again");
+      // If inside range, continue with password requirements 
+    } else {
+      var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
+      var confirmNumericCharacter= confirm("Click OK to confirm if you would like to include numeric characters");    
+      var confirmLowerCase= confirm("Click OK to confirm if you would like to include lowercase characters");
+      var confirmUpperCase= confirm("Click OK to confirm if you would like to include uppercase characters");
+        // If neither, loop to ask the question again 
+        alert("You have to at least choose one");
+
+// Function Return 
+return password; 
+
+// Event Listener 
 generateBtn.addEventListener("click", writePassword);
-
-
-
-// Determine how many characters user wants in password 
-var characters = prompt("How many characters would you like your password to contain?");
-
-// If outside 8-50 units, ask to enter a new number within these parameters 
-if (characters < 8 || characters > 50) {
- alert("Password length must be between 8-50 characters");
- prompt("How many characters would you like your password to contain?");
- // If within 8-50 units, record the number 
-} else if (characters > 8 || characters < 51) {
-  console.log(`I want ${characters} characters in my password`);
-}
-// NEED TO LOOP IF DON'T GIVE CORRECT # 
-
-  
-// Determine how many characters user wants in special characters
-var specialCharacter = true; 
-var questionOne = confirm("Click OK to confirm if you would like to include special characters");
-
- if (questionOne === specialCharacter) {
-  console.log("I do want special characters");
-} else {
-  console.log("I do NOT want special characters");
-}
-
-// Determine how many characters user wants numeric characters
-var numericCharacter = true;
-var questionTwo= confirm("Click OK to confirm if you would like to include numeric characters");
-
-
-if (questionTwo === numericCharacter) {
-  console.log("I do want numeric characters");
-} else {
-  console.log("I do NOT want numeric characters");
-}
-
-// Determine if users would like lowercase characters 
-var lowerCase = true;
-var questionThree= confirm("Click OK to confirm if you would like to include lowercase characters");
-
-
-if (questionThree === lowerCase) {
-console.log("I do want lowercase characters");
-} else {
-  console.log("I do NOT want lowercase characters");
-}
-
-// Determine if users would like uppercase characters 
-var upperCase = true;
-var questionFour= confirm("Click OK to confirm if you would like to include uppercase characters");
-
-
-if (questionFour === upperCase) {
-console.log("I do want uppercase characters");
-} else {
-  console.log("I do NOT want uppercase characters");
-}
-
-
-
-// If neither, loop to ask the question again 
-alert("You have to at least choose one");
-
-console.log("I do NOT want numeric characters");
-
-// Generate Password 
