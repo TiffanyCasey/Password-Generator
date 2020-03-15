8464c122eaae8f86093fe2f837e8ff27987012b8
“TiffanyCasey” <tiffanylynnecasey@gmail.com>
2020-03-15 13:39:14 -0400

    added event listener so prompts start when button is pushed

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 // The Password generator will provide a password with 8-50  characters based on criteria the user specifies.
 
-// Assignment Code 
-var generateBtn = document.querySelector("#generate");
+// Various Arrays 
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
 
 // Variable Declaration 
 var confirmLength
@@ -10,39 +14,40 @@ var confirmNumericCharacter
 var confirmUpperCase
 var confirmLowerCase 
 
-// Various Arrays 
-var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
-var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-// Begin with Button push
-var link = document.getElementById(generate)
+// NOT SURE WHAT THIS IS 
+var link = document.getElementById(generate);
+
+
+// Assignment Code + Event Listener to prompt questions when button pushed
+document.querySelector("#generate").addEventListener("click", promptMe);
 
 // Prompt to confirm how many characters the user would like in their password
-var confirmLength = prompt("How many characters would you like your password to contain?");
+function promptMe(){
+  var confirmLength = prompt("How many characters would you like your password to contain?");
 
-// Loop if answer is outside the parameters 
-while(confirmLength <= 7 || confirmLength >= 51) {
-    alert("Password length must be between 8-50 characters Try again");
-    var confirmLength = prompt("How many characters would you like your password to contain?");
-} 
+  // Loop if answer is outside the parameters 
+  while(confirmLength <= 7 || confirmLength >= 51) {
+      alert("Password length must be between 8-50 characters Try again");
+      var confirmLength = prompt("How many characters would you like your password to contain?");
+  } 
 
-// Repeat back how many charactes the user will have  
-alert(`Your password will have ${confirmLength} characters`);
+  // Repeat back how many charactes the user will have  
+  alert(`Your password will have ${confirmLength} characters`);
 
 
-  // Determine parameters of password 
-var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
-var confirmNumericCharacter= confirm("Click OK to confirm if you would like to include numeric characters");    
-var confirmLowerCase= confirm("Click OK to confirm if you would like to include lowercase characters");
-var confirmUpperCase= confirm("Click OK to confirm if you would like to include uppercase characters");
-// Loop if answer is outside the parameters 
-while(confirmUpperCase === false && confirmLowerCase === false) {
-    alert("You must choose at least upper case or lower case");
-    var confirmLowerCase= confirm("Click OK to confirm if you would like to include lowercase characters");
-    var confirmUpperCase= confirm("Click OK to confirm if you would like to include uppercase characters");
-} 
+    // Determine parameters of password 
+  var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
+  var confirmNumericCharacter= confirm("Click OK to confirm if you would like to include numeric characters");    
+  var confirmLowerCase= confirm("Click OK to confirm if you would like to include lowercase characters");
+  var confirmUpperCase= confirm("Click OK to confirm if you would like to include uppercase characters");
+  // Loop if answer is outside the parameters 
+  while(confirmUpperCase === false && confirmLowerCase === false) {
+      alert("You must choose at least upper case or lower case");
+      var confirmLowerCase= confirm("Click OK to confirm if you would like to include lowercase characters");
+      var confirmUpperCase= confirm("Click OK to confirm if you would like to include uppercase characters");
+  } 
+}
 
   // Begin function to write password to #password input 
   function writePassword() {
@@ -96,7 +101,4 @@ while(confirmUpperCase === false && confirmLowerCase === false) {
 // Return the value
 
 
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
